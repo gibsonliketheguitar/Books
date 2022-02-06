3a0e0b68dfb60092d3d459efeae3c5144ab195d7
Gibson <gibsonthdev@gmail.com>
2022-02-06 11:15:19 +0700

    change name to usd for clarity

--- Refactor_Martin_Fowler/C1/Refactor/printBill.js
+++ Refactor_Martin_Fowler/C1/Refactor/printBill.js
@@ -8,22 +8,16 @@ function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     // add volume credits
     volumeCredits = volumeCreditsFor(perf);
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
